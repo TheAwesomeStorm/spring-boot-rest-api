2ef353173bb9bc5b2866172f213eb0c4aa8da9e2
Daniel Mendes Sechinske <dms@tokenizadora.com.br>
2023-01-26 11:58:57 -0300

    (feature) Added endpoint to list all physicians

--- src/main/java/alura/spring/boot/api/resources/physician/PhysicianController.java
+++ src/main/java/alura/spring/boot/api/resources/physician/PhysicianController.java
@@ -1,14 +1,14 @@
 package alura.spring.boot.api.resources.physician;
 
 import alura.spring.boot.api.resources.physician.dto.CreatePhysicianDto;
+import alura.spring.boot.api.resources.physician.dto.ReadPhysicianDto;
 import alura.spring.boot.api.resources.physician.entities.Physician;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/physicians")
@@ -19,5 +19,10 @@ public class PhysicianController {
     @Transactional
     public void createPhysician(@RequestBody @Valid CreatePhysicianDto dto) {
         repository.save(new Physician(dto));
+    }
+
+    @GetMapping
+    public List<ReadPhysicianDto> readAllPhysicians() {
+        return repository.findAll().stream().map(ReadPhysicianDto::new).toList();
     }
 }
---
+++ src/main/java/alura/spring/boot/api/resources/physician/dto/ReadPhysicianDto.java
@@ -0,0 +1,22 @@
+package alura.spring.boot.api.resources.physician.dto;
+
+import alura.spring.boot.api.resources.enums.Specialty;
+import alura.spring.boot.api.resources.physician.entities.Physician;
+
+public record ReadPhysicianDto(
+        Long id,
+        String name,
+        String email,
+        String code,
+        Specialty specialty
+) {
+    public ReadPhysicianDto(Physician physician) {
+        this(
+                physician.getId(),
+                physician.getName(),
+                physician.getEmail(),
+                physician.getCode(),
+                physician.getSpecialty()
+        );
+    }
+}
